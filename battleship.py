ac420692082ff4c05f950e5f29a61d4871164220
Sai Mrudula Nomula <saimrudulanomula@msitprogram.net>
2021-09-25 20:25:59 +0530

    completed emptyGrid

--- battleship.py
+++ battleship.py
@@ -277,4 +277,4 @@ def runSimulation(w, h):
 if __name__ == "__main__":
     test.testEmptyGrid()
     ## Finally, run the simulation to test it manually ##
-    # runSimulation(500, 500)
+    #runSimulation(500, 500)
