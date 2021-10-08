eabd916a3b25b05512e1d739b0dad3b0270fe4b3
Sai Mrudula Nomula <saimrudulanomula@msitprogram.net>
2021-10-08 12:51:40 +0530

    changes made in drawGameOver to get text on board

--- battleship.py
+++ battleship.py
@@ -336,14 +336,14 @@ Returns: None
 '''
 def drawGameOver(data, canvas):
     if(data["winner"]=="user"):
-        canvas.create_text(100, 50, text="congratulations... you won the game!", fill="black", font=("Arial 15 bold"))
-        canvas.create_text(150, 70, text="press enter and play again", fill="black", font=("Arial 15 bold"))
+        canvas.create_text(200, 50, text="congratulations... you won the game!", fill="black", font=("Arial 15 bold"))
+        canvas.create_text(170, 70, text="press enter and play again", fill="black", font=("Arial 15 bold"))
     if(data["winner"]=="comp"):
-        canvas.create_text(100, 50, text="You lost the game...Try again!", fill="black", font=("Arial 15 bold"))
-        canvas.create_text(150, 70, text="press enter and play again", fill="black", font=("Arial 15 bold"))
+        canvas.create_text(200, 50, text="You lost the game...Try again!", fill="black", font=("Arial 15 bold"))
+        canvas.create_text(170, 70, text="press enter and play again", fill="black", font=("Arial 15 bold"))
     if(data["winner"]=="draw"):
-        canvas.create_text(100, 50, text="out of moves...draw match", fill="black", font=("Arial 15 bold"))
-        canvas.create_text(150, 70, text="press enter and play again", fill="black", font=("Arial 15 bold"))
+        canvas.create_text(200, 50, text="out of moves...draw match", fill="black", font=("Arial 15 bold"))
+        canvas.create_text(170, 70, text="press enter and play again", fill="black", font=("Arial 15 bold"))
     return
 
 
